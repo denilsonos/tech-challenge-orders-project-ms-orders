1493cf72b751b1d185895b6f6ae18afa14f6caeb
Helio Soares <helio_soares@outlook.com>
2024-05-19 18:14:09 -0300

    #Adjust for test

--- src/adapters/external-services/preparation-client/preparation-client.ts
+++ src/adapters/external-services/preparation-client/preparation-client.ts
@@ -6,13 +6,11 @@ import { OrderStatus } from "../../../core/entities/enums/order-status";
 export class PreparationClient implements PreparationClientAdapter {
   constructor() { }
   async createOrderPreparation(orderId: number, status: string): Promise<void> {
-    console.log("status:"+status)
     const basePath = process.env.PREPARATION_MS_HOST;
     await axios.post(`${basePath}/ms-preparation/api/v1/orders`, {
-       data: { 
          idOrder: Number(orderId),
          status: String(OrderStatus.Created),
-         createdAt: new Date()} 
+         createdAt: new Date()
       })
       .then(response => console.log("response status:" + response))
       .catch(error => console.log("error status:" + error));
